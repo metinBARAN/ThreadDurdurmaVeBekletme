ed7ec23320f63a5ce8ecca69af5efcae5b5deabc
metinBARAN <metinbaran0000@gmail.com>
2023-06-15 12:36:53 +0300

    Thread Durdurma ve Bekletme

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter c1=new Counter("c1",1);
+        Thread t1=new Thread(c1);
+
+        t1.start();
+        Thread.sleep(2000);
+        c1.stop();
+
+
+    }
+}
